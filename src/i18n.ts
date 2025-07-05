a8c73079c9a37257288dd566e7a3d6ea4aca75ac
Jesus Vizcarra <jesus@cococo.ventures>
2025-07-05 00:18:06 +0000

    next-intl

--- src/i18n.ts
+++
@@ -1,5 +0,0 @@
-import {getRequestConfig} from 'import { useTranslations } from "next-intl";ntl/server';
- 
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
